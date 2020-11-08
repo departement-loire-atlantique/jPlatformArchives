f1838adb55d46805ef22adf0436ae159f65bb3c0
sguellec <stephane.guellec@loire-atlantique.fr>
2020-11-08 11:23:50 +0100

    Hotfix : rajout de méthode + changement appels méthodes utilitaires
    - Rajout de la méthode "envoiMailCommunicationAdministrativeArchives" qui était initialement dans Socle.
    - Appel des méthodes "sendMail" en invoquant la classe "MailUtils" de Socle
    - Suppression de l'héritage de Mailutils.

--- WEB-INF/classes/fr/cg44/plugin/archives/ArchivesMailUtils.java
+++ WEB-INF/classes/fr/cg44/plugin/archives/ArchivesMailUtils.java
@@ -13,17 +13,76 @@ import com.jalios.util.Util;
 import fr.cg44.plugin.socle.MailUtils;
 import fr.cg44.plugin.socle.SocleUtils;
 import generated.AutreRechercheForm;
+import generated.CommunicationForm;
 import generated.RechercheJugementForm;
 
-public final class ArchivesMailUtils extends MailUtils {
+public final class ArchivesMailUtils {
   private static Channel channel = Channel.getChannel();
 
   private static final Logger LOGGER = Logger.getLogger(ArchivesMailUtils.class);
 
-  public ArchivesMailUtils() {
+  private ArchivesMailUtils() {
     throw new IllegalStateException("Utility class");
   }
 
+  /**
+   * Envoi de l'email du formulaire de communication administrative (site Archives)
+   */
+  public static boolean envoiMailCommunicationAdministrativeArchives(CommunicationForm form) {
+    
+    boolean result = false;
+    String jsp = "/plugins/SoclePlugin/jsp/mail/formulaireArchivesCommunicationAdministrativeTemplate.jsp";
+    String date = SocleUtils.formatDate("dd/MM/yy HH:mm", new Date());
+    
+    // Objet
+    String objet = JcmsUtil.glpd("jcmsplugin.archives.email.communication.objet");
+    
+    // Destinataire / emetteur
+    String emailTo = channel.getProperty("jcmsplugin.archives.form.mailTo");
+    String emailFrom = channel.getProperty("jcmsplugin.archives.form.from");
+    
+    // CC
+    ArrayList<String> listeEmailCC = new ArrayList<>();
+    if(Util.notEmpty(form.getCourriel())){
+      listeEmailCC.add(form.getCourriel());
+    }
+    
+    // Contenu
+    HashMap<Object, Object> parametersMap = new HashMap<Object, Object>();
+    parametersMap.put("nom", form.getNom());
+    parametersMap.put("prenom", form.getPrenom());
+    parametersMap.put("administration", form.getAdministration());
+    parametersMap.put("direction", form.getDirection());
+    parametersMap.put("service", form.getServiceUnite());
+    parametersMap.put("adresse", form.getAdresse());
+    parametersMap.put("complementAdresse", form.getComplementDadresse());
+    parametersMap.put("codePostal", form.getCodePostal());
+    parametersMap.put("ville", form.getVille());
+    parametersMap.put("telephone", form.getTelephone());
+    parametersMap.put("courriel", form.getCourriel());
+    parametersMap.put("versement", form.getDateDeVersement());
+    parametersMap.put("dossier", form.getNumeroDuDossier());
+    parametersMap.put("natureRecherche", form.getNatureDeLaRecherche());
+    parametersMap.put("dateVersement", form.getDateDeVersement());
+    parametersMap.put("dateRetour", form.getRetourDuDossierPrevuLe());
+    parametersMap.put("date", date);
+
+    if (Util.notEmpty(emailFrom) && Util.notEmpty(emailTo)) {
+      try {
+        MailUtils.sendMail(objet, null, emailFrom, emailTo, listeEmailCC, null, jsp, parametersMap);
+        result = true;
+      } catch (Exception e) {
+        result = false;
+        LOGGER.error("Formulaire de communication administrative - Erreur lors de l'envoi du mail : " + e.getMessage());
+      }
+
+    } else {
+      result = false;
+      LOGGER.error("Formulaire de communication administrative - Paramètres d'envois (emetteur / destinataire) non définis.");
+    }
+
+    return result;
+  }    
 
   /**
    * Envoi de l'email du formulaire de recherche de jugement (site Archives)
@@ -71,7 +130,7 @@ public final class ArchivesMailUtils extends MailUtils {
 
     if (Util.notEmpty(emailFrom) && Util.notEmpty(emailTo)) {
       try {
-        sendMail(objet, null, emailFrom, emailTo, listeEmailCC, null, jsp, parametersMap);
+        MailUtils.sendMail(objet, null, emailFrom, emailTo, listeEmailCC, null, jsp, parametersMap);
         result = true;
       } catch (Exception e) {
         result = false;
@@ -123,7 +182,7 @@ public final class ArchivesMailUtils extends MailUtils {
 
     if (Util.notEmpty(emailFrom) && Util.notEmpty(emailTo)) {
       try {
-        sendMail(objet, null, emailFrom, emailTo, listeEmailCC, null, jsp, parametersMap);
+        MailUtils.sendMail(objet, null, emailFrom, emailTo, listeEmailCC, null, jsp, parametersMap);
         result = true;
       } catch (Exception e) {
         result = false;
