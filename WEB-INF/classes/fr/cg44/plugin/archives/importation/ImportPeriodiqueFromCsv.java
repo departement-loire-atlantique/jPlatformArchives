f589b602f8ff7e00f3efbb79bf136d6d45744013
LChoquet <lucas.choquet@trsb.net>
2021-12-13 09:37:44 +0100

    2014 - retours recette import périodique

--- WEB-INF/classes/fr/cg44/plugin/archives/importation/ImportPeriodiqueFromCsv.java
+++ WEB-INF/classes/fr/cg44/plugin/archives/importation/ImportPeriodiqueFromCsv.java
@@ -15,7 +15,6 @@ import com.csvreader.CsvReader;
 import com.jalios.jcms.Category;
 import com.jalios.jcms.Channel;
 import com.jalios.jcms.ControllerStatus;
-import com.jalios.jcms.FileDocument;
 import com.jalios.jcms.Publication;
 import com.jalios.jcms.context.JcmsMessage;
 import com.jalios.util.Util;
@@ -36,25 +35,18 @@ public class ImportPeriodiqueFromCsv {
     /*
      * Liste des valeurs attendues dans le CSV
      * 0 Titre - String - obligatoire
-     * 1 Image bandeau - String
-     * 2 Copyright - String
-     * 3 Légende - String
-     * 4 Texte alternatif - String
-     * 5 Cote de périodique - String - obligatoire
-     * 6 Dates extrêmes de la collection - String - Obligatoire
-     * 7 Dates extrêmes de la collection numérisée - String
-     * 8 Inventaire de la collection - FileDocument
-     * 9 Fréquence parution - Category - Obligatoire
-     * 10 Ancien titre - Periodique[]
-     * 11 Titre devenu - Periodique[]
-     * 12 Notes - String
-     * 13 Siège du journal - City
-     * 14 Numérisé - boolean oui/non
-     * 15 Thème - Category[] - Obligatoire
-     * 16 Tendances politiques - Category[]
-     * 17 Période - Category[] - Obligatoire
-     * 18 Communes concernées - City[]
-     * 19 Toutes les communes du département - boolean oui/non
+     * 1 Cote de périodique - String - obligatoire
+     * 2 Dates extrêmes de la collection - String - Obligatoire
+     * 3 Dates extrêmes de la collection numérisée - String
+     * 4 Fréquence parution - Category - Obligatoire
+     * 5 Notes - String
+     * 6 Siège du journal - City
+     * 7 Numérisé - boolean oui/non
+     * 8 Thème - Category[] - Obligatoire
+     * 9 Tendances politiques - Category[]
+     * 10 Période - Category[] - Obligatoire
+     * 11 Communes concernées - City[]
+     * 12 Toutes les communes du département - boolean oui/non
      */
     
     /**
@@ -77,17 +69,14 @@ public class ImportPeriodiqueFromCsv {
                      * Champs à surveiller :
                      * 
                      * 0 Titre - String - obligatoire
-                     * 5 Cote de périodique - String - obligatoire
-                     * 6 Dates extrêmes de la collection - String - Obligatoire
-                     * 8 Inventaire de la collection - FileDocument
-                     * 9 Fréquence parution - Category - Obligatoire
-                     * 10 Ancien titre - Periodique[]
-                     * 11 Titre devenu - Periodique[]
-                     * 13 Siège du journal - City
-                     * 15 Thème - Category[] - Obligatoire
-                     * 16 Tendances politiques - Category[]
-                     * 17 Période - Category[] - Obligatoire
-                     * 18 Communes concernées - City[]
+                     * 1 Cote de périodique - String - obligatoire
+                     * 2 Dates extrêmes de la collection - String - Obligatoire
+                     * 4 Fréquence parution - Category - Obligatoire
+                     * 6 Siège du journal - City
+                     * 8 Thème - Category[] - Obligatoire
+                     * 9 Tendances politiques - Category[]
+                     * 10 Période - Category[] - Obligatoire
+                     * 11 Communes concernées - City[]
                      */
                     lineLog = new StringBuilder();
                     values = csvReader.getValues();
@@ -102,37 +91,28 @@ public class ImportPeriodiqueFromCsv {
                     if (Util.isEmpty(values[0])) { // titre
                         lineLog.append("Le titre (champ obligatoire) est vide. # ");
                     }
-                    if (Util.isEmpty(values[5])) { // cote de périodique
+                    if (Util.isEmpty(values[1])) { // cote de périodique
                         lineLog.append("La cote de périodique (champ obligatoire) est vide. # ");
                     }
-                    if (Util.isEmpty(values[6])) { // dates extrêmes de la collection
+                    if (Util.isEmpty(values[2])) { // dates extrêmes de la collection
                         lineLog.append("Les dates extrêmes de la collection (champ obligatoire) sont vides. # ");
                     }
-                    if (Util.notEmpty(values[8]) ? !contentCheck(values[8],FileDocument.class) : false) { // Inventaire de la collection
-                        lineLog.append("Le champ Inventaire de la collection présente un contenu incorrect (il n'existe pas ou ne pointe pas sur un document) # ");
-                    }
-                    if (Util.notEmpty(values[9]) ? !categoryCheck(values[9], channel.getCategory("$jcmsplugin.archives.category.periodique.frequenceparution")) : false) { // Fréquence parution
+                    if (Util.notEmpty(values[4]) ? !categoryCheck(values[6], channel.getCategory("$jcmsplugin.archives.category.periodique.frequenceparution")) : false) { // Fréquence parution
                         lineLog.append("Le champ Fréquence parution présente une catégorie incorrecte (mauvaise branche ou ID n'existant pas) # ");
                     }
-                    if (Util.notEmpty(values[10]) ? !contentCheck(values[10], Periodique.class) : false) { // Ancien titre
-                        lineLog.append("Le champ Ancien titre présente des contenus incorrects (un ou plusieurs contenus n'existent pas / ne sont pas des Périodiques) # ");
-                    }
-                    if (Util.notEmpty(values[11]) ? !contentCheck(values[11], Periodique.class) : false) { // Titre devenu
-                        lineLog.append("Le champ Titre devenu présente des contenus incorrects (un ou plusieurs contenus n'existent pas / ne sont pas des Périodiques) # ");
-                    }
-                    if (Util.notEmpty(values[13]) ? !contentCheck(values[13],City.class) : false) { // Siège du journal
+                    if (Util.notEmpty(values[6]) ? !contentCheck(values[6],City.class) : false) { // Siège du journal
                         lineLog.append("Le champ Siège du journal présente un contenu incorrect (il n'existe pas ou ne pointe pas sur une commune) # ");
                     }
-                    if (Util.notEmpty(values[15]) ? !categoryCheck(values[15], channel.getCategory("$jcmsplugin.archives.category.periodique.theme")) : false) { // Thème
+                    if (Util.notEmpty(values[8]) ? !categoryCheck(values[8], channel.getCategory("$jcmsplugin.archives.category.periodique.theme")) : false) { // Thème
                         lineLog.append("Le champ Thème présente une ou des catégories incorrectes (mauvaise branche ou IDs n'existant pas) # ");
                     }
-                    if (Util.notEmpty(values[16]) ? !categoryCheck(values[16], channel.getCategory("$jcmsplugin.archives.category.periodique.tendancespolitiques")) : false) { // Tendances politiques
+                    if (Util.notEmpty(values[9]) ? !categoryCheck(values[9], channel.getCategory("$jcmsplugin.archives.category.periodique.tendancespolitiques")) : false) { // Tendances politiques
                         lineLog.append("Le champ Tendances politiques présente une ou des catégories incorrectes (mauvaise branche ou IDs n'existant pas) # ");
                     }
-                    if (Util.notEmpty(values[17]) ? !categoryCheck(values[17], channel.getCategory("$jcmsplugin.archives.category.periodique.periode")) : false) { // Période
+                    if (Util.notEmpty(values[10]) ? !categoryCheck(values[10], channel.getCategory("$jcmsplugin.archives.category.periodique.periode")) : false) { // Période
                         lineLog.append("Le champ Période présente une ou des catégories incorrectes (mauvaise branche ou IDs n'existant pas) # ");
                     }
-                    if (Util.notEmpty(values[18]) ? !contentCheck(values[18], City.class) : false) { // Communes concernées
+                    if (Util.notEmpty(values[11]) ? !contentCheck(values[11], City.class) : false) { // Communes concernées
                         lineLog.append("Le champ Communes concernées présente des contenus incorrects (un ou plusieurs contenus n'existent pas / ne sont pas des Communes) # ");
                     }
                     
@@ -250,91 +230,51 @@ public class ImportPeriodiqueFromCsv {
         Periodique itPeriodique = new Periodique();
         // Titre
         itPeriodique.setTitle(values[0]);
-        /*
-        * 18 Communes concernées - City[]
-        * 19 Toutes les communes du département - boolean oui/non
-        */
         
-        // Image bandeau
-        if (Util.notEmpty(values[1])) {
-            itPeriodique.setImageBandeau(values[1]);
-        }
-        // Copyright
-        if (Util.notEmpty(values[2])) {
-            itPeriodique.setCopyright(values[2]);
-        }
-        // Légende
-        if (Util.notEmpty(values[3])) {
-            itPeriodique.setLegende(values[3]);
-        }
-        // Texte alternatif
-        if (Util.notEmpty(values[4])) {
-            itPeriodique.setTexteAlternatif(values[4]);
-        }
         // Cote de périodique
-        itPeriodique.setCoteDePeriodique(values[5]);
+        itPeriodique.setCoteDePeriodique(values[1]);
         // Dates extrêmes de la collection 
-        itPeriodique.setDatesExtremesDeLaCollection(values[6]);
+        itPeriodique.setDatesExtremesDeLaCollection(values[2]);
         // Dates extrêmes de la collection numérisée
-        if (Util.notEmpty(values[7])) {
-            itPeriodique.setTexteAlternatif(values[7]);
-        }
-        // Inventaire de la collection
-        if (Util.notEmpty(values[8])) {
-            itPeriodique.setInventaireDeLaCollection(channel.getData(FileDocument.class, values[8]));
+        if (Util.notEmpty(values[3])) {
+            itPeriodique.setTexteAlternatif(values[3]);
         }
         // Fréquence parution
-        itPeriodique.addCategory(channel.getCategory(values[9]));
-        // Ancien titre
-        if (Util.notEmpty(values[10])) {
-            List<Periodique> periodiques = new ArrayList<>();
-            for (String itId : values[10].split(doubleHashtag)) {
-                periodiques.add(channel.getData(Periodique.class, itId));
-            }
-            itPeriodique.setAncienTitre(periodiques.toArray(new Periodique[periodiques.size()]));
-        }
-        // Titre devenu
-        if (Util.notEmpty(values[11])) {
-            List<Periodique> periodiques = new ArrayList<>();
-            for (String itId : values[11].split(doubleHashtag)) {
-                periodiques.add(channel.getData(Periodique.class, itId));
-            }
-            itPeriodique.setTitreDevenu(periodiques.toArray(new Periodique[periodiques.size()]));
-        }
+        itPeriodique.addCategory(channel.getCategory(values[4]));
         // Notes
-        if (Util.notEmpty(values[12])) {
-            itPeriodique.setNotes(values[12]);
+        if (Util.notEmpty(values[5])) {
+            itPeriodique.setNotes(values[5]);
         }
         // Siège
-        if (Util.notEmpty(values[13])) {
-            itPeriodique.setSiegeDuJournal(channel.getData(City.class, values[13]));
+        if (Util.notEmpty(values[6])) {
+            itPeriodique.setSiegeDuJournal(channel.getData(City.class, values[6]));
         }
         // Numérisé
-        itPeriodique.setNumerise("oui".equalsIgnoreCase(values[14]));
+        itPeriodique.setNumerise("oui".equalsIgnoreCase(values[7]));
         // Thème
-        for (String itCid : values[15].split(doubleHashtag)) {
+        for (String itCid : values[8].split(doubleHashtag)) {
             itPeriodique.addCategory(channel.getCategory(itCid));
         }
         // Tendances politiques
-        if (Util.notEmpty(values[16])) {
-            for (String itCid : values[16].split(doubleHashtag)) {
+        if (Util.notEmpty(values[9])) {
+            for (String itCid : values[9].split(doubleHashtag)) {
                 itPeriodique.addCategory(channel.getCategory(itCid));
             }
         }
         // Période
-        for (String itCid : values[17].split(doubleHashtag)) {
+        for (String itCid : values[10].split(doubleHashtag)) {
             itPeriodique.addCategory(channel.getCategory(itCid));
         }
         // Communes concernées
-        if (Util.notEmpty(values[18])) {
+        if (Util.notEmpty(values[11])) {
             List<City> communes = new ArrayList<>();
-            for (String itId : values[18].split(doubleHashtag)) {
+            for (String itId : values[11].split(doubleHashtag)) {
                 communes.add(channel.getData(City.class, itId));
             }
             itPeriodique.setCommunesConcernees(communes.toArray(new City[communes.size()]));
         }
         // Toutes les communes
-        itPeriodique.setToutesLesCommunesDuDepartement("oui".equalsIgnoreCase(values[19]));
+        itPeriodique.setToutesLesCommunesDuDepartement("oui".equalsIgnoreCase(values[12]));
         
         itPeriodique.setAuthor(channel.getCurrentLoggedMember());
         
